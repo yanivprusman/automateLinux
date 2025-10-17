7837ea6d6b12a13fe7a89d6eee4393af6c74b552
yaniv <yanivprusman@gmail.com>
2025-10-17 21:57:30 +0300

    wip

--- mouse_to_enter.py
+++ mouse_to_enter.py
@@ -10,13 +10,41 @@ BTN_FORWARD = 277  # evtest code for BTN_FORWARD
 # Open mouse device
 mouse = evdev.InputDevice(MOUSE_DEVICE)
 
-with uinput.Device([uinput.KEY_ENTER]) as ui:
-    print('Listening for BTN_FORWARD events...')
-    for event in mouse.read_loop():
-        if event.type == evdev.ecodes.EV_KEY and event.code == BTN_FORWARD:
-            if event.value == 1:  # Key press
-                ui.emit(uinput.KEY_ENTER, 1)  # Press Enter
-            elif event.value == 0:  # Key release
-                ui.emit(uinput.KEY_ENTER, 0)  # Release Enter
-        time.sleep(0.001)
+# with uinput.Device([uinput.KEY_1]) as ui:
+#     print('Listening for BTN_FORWARD events...')
+#     for event in mouse.read_loop():
+#         if event.type == evdev.ecodes.EV_KEY and event.code == BTN_FORWARD:
+#             if event.value == 1:  # Key press
+#                 ui.emit(uinput.KEY_1, 1)  # Press Enter
+#                 ui.emit(uinput.KEY_1, 0)  # Press Enter
+#                 time.sleep(0.05)  
+#                 ui.emit(uinput.KEY_1, 1)  # Press Enter
+#                 ui.emit(uinput.KEY_1, 0)  # Press Enter
+#                 time.sleep(0.05)  
+#                 ui.emit(uinput.KEY_1, 1)  # Press Enter
+#                 ui.emit(uinput.KEY_1, 0)  # Press Enter
+#                 time.sleep(0.05)
+#                 ui.emit(uinput.KEY_1, 1)  # Press Enter
+#                 ui.emit(uinput.KEY_1, 0)  # Press Enter
+#             elif event.value == 0:  # Key release
+#                 ui.emit(uinput.KEY_1, 1)  # Press Enter
+#                 ui.emit(uinput.KEY_1, 0)  # Press Enter
+#                 time.sleep(0.05)
+#                 ui.emit(uinput.KEY_1, 1)  # Press Enter
+#                 ui.emit(uinput.KEY_1, 0)  # Press Enter
+#                 time.sleep(0.05)
+#                 ui.emit(uinput.KEY_1, 1)  # Press Enter
+#                 ui.emit(uinput.KEY_1, 0)  # Press Enter
+#                 time.sleep(0.05)
+#                 ui.emit(uinput.KEY_1, 1)  # Press Enter
+#                 ui.emit(uinput.KEY_1, 0)  # Press Enter
+#         time.sleep(0.001)
+# print (uinput.KEY_1)
 # print (uinput.KEY_ENTER)
+
+# with uinput.Device([uinput.KEY_1]) as ui:
+#     for event in mouse.read_loop():
+#         if event.type == evdev.ecodes.EV_KEY and event.code == BTN_FORWARD:
+#             print("Asdf")
+#         else:
+#             print("AAAA")

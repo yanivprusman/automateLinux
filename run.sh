21ac2d253a33d8e1338ecef9f8c9f20d26efce04
yaniv <yanivprusman@gmail.com>
2025-10-18 12:35:49 +0300

    '1' working

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 sudo evsieve --input /dev/input/event6 grab \
-    --map btn:forward:1 key:enter:1 \
-    --map btn:forward:0 key:enter:0 \
+    --map btn:forward:1 key:1:1 \
+    --map btn:forward:0 key:1:0 \
     --output

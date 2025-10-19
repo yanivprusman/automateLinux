5de03ca50d8053e58545811d23abab65473cb249
yaniv <yanivprusman@gmail.com>
2025-10-19 10:48:36 +0300

    added services/enable.sh

--- mappings/corseairKeyboard.sh
+++ mappings/corseairKeyboard.sh
@@ -1,11 +1,16 @@
 KEYBOARD_BY_ID=$(ls /dev/input/by-id/ | grep 'Corsair.*-event-kbd')
 
+
+sudo evsieve --input /dev/input/by-id/$KEYBOARD_BY_ID \
+    --hook key:leftctrl key:a send-key=key:s \
+    --withhold key:a  \
+    --output
+
 sudo evsieve --input /dev/input/by-id/$KEYBOARD_BY_ID grab \
     --map key:f2:1 key:leftctrl:1 key:s:1 key:s:0 key:leftctrl:0 \
     --map key:f3:1 key:leftctrl:1 key:y:1 key:y:0 key:leftctrl:0 \
     --map key:f4:1 key:leftctrl:1 key:z:1 key:z:0 key:leftctrl:0 \
     --output name="evsieve keyboard"
 
-    
 
-    
+d
---
+++ services/enable.sh
@@ -0,0 +1,2 @@
+sudo ln -s /home/yaniv/coding/automationLinux/services/run-mappings.service /etc/systemd/system/run-mappings.service
+

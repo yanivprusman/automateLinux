2e8dc3dfa5ead572ae6725f58b3185f4a55b1c8d
yaniv <yanivprusman@gmail.com>
2025-10-20 10:19:34 +0300

    wip

--- test.sh
+++ test.sh
@@ -2,35 +2,41 @@
 # KEYBOARD_BY_ID=$(ls /dev/input/by-id/ | grep 'Corsair.*-event-kbd')
 # sudo evsieve --input /dev/input/by-id/$KEYBOARD_BY_ID grab \
 sudo evsieve --input /dev/input/by-id/evsieveKeyboardPhase2 domain=evsieveKeyboardPhase2 grab \
-    --map key:a key:a key:s key:d key:f \
-    --map key:q         @devNull \
-    --map key:w         @devNull \
-    --map key:rightctrl @devNull \
-    --map key:leftctrl  @devNull \
-    --map key:leftalt   @devNull \
-    --map key:leftshift @devNull \
-    --map key:backslash @devNull \
-    --map key:enter     @devNull \
-    --map key:e         @devNull \
-    --map key:k         @devNull \
-    --map key:y         @devNull \
-    --map key:1         @devNull \
-    --map key:2         @devNull \
-    --map key:3         @devNull \
-    --map key:4         @devNull \
-    --map key:5         @devNull \
-    --map key:6         @devNull \
-    --hook key:e key:w send-key=key:a send-key=key:s send-key=key:d send-key=key:f \
-    --hook key:q key:e send-key=key:a send-key=key:s send-key=key:d send-key=key:f \
-    --hook key:q key:w send-key=key:a send-key=key:s send-key=key:d send-key=key:f \
-    --hook key:leftctrl key:leftalt key:leftshift key:1 send-key=key:enter send-key=key:g send-key=key:1 \
-    --hook key:leftctrl key:leftalt key:leftshift key:2 send-key=key:enter send-key=key:g send-key=key:1 \
-    --hook key:leftctrl key:leftalt key:leftshift key:3 send-key=key:enter send-key=key:g send-key=key:1 \
-    --hook key:leftctrl key:leftalt key:leftshift key:4 send-key=key:enter \
-    --hook key:leftctrl key:leftalt key:leftshift key:5 send-key=key:enter send-key=key:g send-key=key:1 \
-    --hook key:leftctrl key:leftalt key:leftshift key:6 send-key=key:enter send-key=key:g send-key=key:1 \
-    --output @devNull
+    --map key:a         @sparce \
+    --map key:q         @sparce \
+    --map key:w         @sparce \
+    --map key:rightctrl @sparce \
+    --map key:leftctrl  @sparce \
+    --map key:leftalt   @sparce \
+    --map key:leftshift @sparce \
+    --map key:backslash @sparce \
+    --map key:enter     @sparce \
+    --map key:e         @sparce \
+    --map key:k         @sparce \
+    --map key:y         @sparce \
+    --map key:1         @sparce \
+    --map key:2         @sparce \
+    --map key:3         @sparce \
+    --map key:4         @sparce \
+    --map key:5         @sparce \
+    --map key:6         @sparce \
+    --map key:tab         @sparce \
+    --map key:e @devNull \
+    --hook key:e key:w send-key=key:a send-key=key:s send-key=key:d send-key=key:f sequential\
+    --withhold key:w \
+    --hook key:e key:w send-key=key:a send-key=key:s send-key=key:d send-key=key:f sequential\
+    --withhold key:w \
+    --hook key:e key:g key:k key:y key:4 send-key=key:enter sequential\
+    --withhold \
+    --output @sparce
+# egky1
+# egky2
+# egky3
+# egky4
+# egky5
+# egky6
     # (hook) sequential
+    # send-key=key:a send-key=key:s send-key=key:d send-key=key:f \
     # --hook key:leftctrl key:e send-key=key:a send-key=key:s send-key=key:d send-key=key:f \
     # (hook) --withhold
     # --hook key:q key:w exec-shell="echo Hello, world2!" \

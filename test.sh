eade9153490db835b634c2b8dfe4ccced975d4d9
yaniv <yanivprusman@gmail.com>
2025-10-22 20:00:30 +0300

    3 shift

--- test.sh
+++ test.sh
@@ -1,41 +1,47 @@
 # exec 2>/dev/null
 # systemd-run --service-type=notify --unit=keyBoardTest.service \
 systemd-run --collect --service-type=notify --unit=keyBoardTest.service \
-evsieve --input /dev/input/by-id/keyBoard grab domain=regular \
-    --map key:space:1 key:leftctrl:1 key:n:1 key:n:0 key:s:1 key:s:0 key:leftctrl:0 \
+evsieve --input /dev/input/by-id/keyBoard grab \
+    --map msc:scan \
+    --map key:space:1 key:leftctrl:1 key:s:1 key:s:0 key:n:1 key:n:0 key:leftctrl:0 \
     --output  name="keyboardB4Test" create-link=/dev/input/by-id/keyBoardB4Test repeat=disable \
-    # --map key:space:1 key:leftctrl:1 key:n:1 key:n:0 key:s:1 key:s:0 key:leftctrl:0 \
-    # --map key:space:1 key:leftctrl:1 key:n:1 key:n:0 key:leftctrl:0 \
-    # --map key:space:1 key:leftctrl:1 key:s:1 key:s:0 key:leftctrl:0 \ the only one that is working
+    # --map key:space:1 key:leftctrl:1 key:s:1 key:s:0 key:n:1 key:n:0 key:leftctrl:0 \ not working
+    # --map key:space:1 key:leftctrl:1 key:n:1 key:n:0 key:s:1 key:s:0 key:leftctrl:0 \ working
+    # --map key:space:1 key:leftctrl:1 key:n:1 key:n:0 key:leftctrl:0 \                 working
+    # --map key:space:1 key:leftctrl:1 key:s:1 key:s:0 key:leftctrl:0 \                 working
 
 
 sudo evsieve --input /dev/input/by-id/keyBoardB4Test grab domain=regular \
-    --hook "" toggle=regularToDevNull:1 --map key:a:1 key:a:1 \
-    --map msc:scan \
-    --hook key:leftshift key:k key:e key:y key:g key:6 \
-        breaks-on=key::1 send-key=key:leftctrl send-key=key:c sequential \
-    --hook key:leftshift key:k key:e key:y key:g key:5 \
-        breaks-on=key::1 send-key=key:leftctrl send-key=key:v sequential \
-    --hook key:leftshift key:k key:e key:y key:g key:4 \
-        breaks-on=key::1 send-key=key:enter  sequential \
-    --hook key:leftshift key:k key:e key:y key:g key:3 \
-        breaks-on=key::1 send-key=key:leftctrl send-key=key:x sequential \
-    --withhold \
-    --hook key:leftctrl@regular key:e \
-        breaks-on=key::1 toggle=regularToDevNull:2 send-key=key:leftshift@special send-key=key:a@special send-key=key:s@special send-key=key:d@special send-key=key:f@special sequential \
-    --hook key:leftctrl@regular key:r \
-        breaks-on=key::1 toggle=regularToDevNull:2 send-key=key:leftshift@special  send-key=key:s@special send-key=key:d@special send-key=key:f@special send-key=key:g@special sequential \
-    --withhold \
-    --copy key:leftctrl key:leftctrl@copy \
-    --hook key:leftctrl@copy key:s \
-        toggle=regularToDevNull:2 send-key=key:leftctrl@special send-key=key:n@special sequential \
-    --withhold \
-    --hook key:leftctrl@copy key:s \
-        \
-    --withhold \
-    --toggle @regular @regular @devNull id=regularToDevNull\
-    --print format=direct \
-    --output @regular @special create-link=/dev/input/by-id/keyBoardTest repeat=disable \
+    \--map key:leftshift @leftShift \
+    --hook @regular toggle=leftshiftCount:1 \
+    --hook @leftShift toggle=leftshiftCount \
+    --toggle @leftShift @leftShift0 @leftShift1 @leftShift2 @leftShift3 id=leftshiftCount \
+    --hook @leftShift3 \
+        toggle=leftshiftCount send-key=key:leftshift send-key=key:a send-key=key:s send-key=key:d send-key=key:f sequential \
+    --map key @regular \
+    --output @regular create-link=/dev/input/by-id/keyBoardTest repeat=disable \
+    # --copy key:leftshift key:leftshift@copy \
+    # --copy key:leftctrl key:leftctrl@copy \
+    # --hook key:leftshift@copy key:k key:e key:y key:g key:6 \
+    #     breaks-on=key::1 send-key=key:leftctrl@regular send-key=key:c sequential \
+    # --hook key:leftshift@copy key:k key:e key:y key:g key:5 \
+    #     breaks-on=key::1 send-key=key:leftctrl@regular send-key=key:v sequential \
+    # --hook key:leftshift@copy key:k key:e key:y key:g key:4 \
+    #     breaks-on=key::1 send-key=key:enter  sequential \
+    # --hook key:leftshift@copy key:k key:e key:y key:g key:3 \
+    #     breaks-on=key::1 send-key=key:leftctrl@regular send-key=key:x sequential \
+    # --withhold \
+    # --print format=direct \
+    # --hook key:leftctrl@copy key:e \
+    #     breaks-on=key::1 send-key=key:leftshift send-key=key:a send-key=key:s@copy send-key=key:d send-key=key:f sequential \
+    # --hook key:leftctrl@copy key:r \
+    #     breaks-on=key::1 send-key=key:leftshift  send-key=key:s@copy send-key=key:d send-key=key:f send-key=key:g sequential \
+    # --hook key:leftctrl@copy key:s@regular \
+    #     send-key=key:leftctrl@regular send-key=key:n sequential \
+    # --withhold \
+    # --hook key:leftctrl@copy key:s@regular \
+    #     \
+    # --withhold \
 
 sudo systemctl stop keyBoardTest.service 2>/dev/null
 sudo systemctl reset-failed keyBoardTest.service 2>/dev/null

4d0fe373c94051085972e72b6f33f07abf5f354f
yaniv <yanivprusman@gmail.com>
2025-10-22 22:49:02 +0300

    wip

--- _testCopy.sh
+++ _testCopy.sh
@@ -1,32 +1,23 @@
 # exec 2>/dev/null
-sudo evsieve --input /dev/input/by-id/keyBoard grab \
-    --hook key:leftshift key:k key:e key:y key:g key:6 \
-        send-key=key:leftctrl send-key=key:c sequential \
-    --print format=direct \
-    --hook key:leftshift key:k key:e key:y key:g key:5 \
-        send-key=key:leftctrl send-key=key:v sequential \
-    --withhold \
-    \
-    --toggle key:leftctrl:1 key:leftctrl:1 key:leftctrl:0 id=leftctrl \
-    --toggle @regular @regular @devNull id=regularToDevNull \
-    --output 
-# 
-    # --hook key:leftctrl key:e toggle=leftctrl:2 send-key=key:leftshift send-key=key:a send-key=key:s send-key=key:d send-key=key:f sequential \
-    # --withhold \
-    # --print format=direct \
-    # --withhold \
-    # --withhold \
-    # --hook key:leftshift key:k key:e key:y key:g key:4 send-key=key:enter sequential \
-    # --hook key:leftshift key:k key:e key:y key:g key:3 send-key=key:leftctrl send-key=key:x sequential \
-    # --hook key:leftshift key:k key:e key:y key:g key:2 send-key=key:leftctrl send-key=key:a send-key=key:c sequential \
-    # --hook key:leftshift key:k key:e key:y key:g key:1 send-key=key:leftctrl send-key=key:f sequential \
-# ssdfg sdfg sddddd sddddd sdfg sdd6keyg key:g key:gdd cDF ASDFggggg
-
-# 6 copy                KEYG^   6keyg
-# 5 paste               KEYG% 
-# 4 enter               KEYG$   4keyg
-# 3 cut                 KEYG#   3keyg
-# 2 select all and copy KEYG@   2keyg
-# 1 copy and search     KEYG!   1keyg
+# systemd-run --service-type=notify --unit=keyBoardTest.service \
+systemd-run --collect --service-type=notify --unit=keyBoardTest.service \
+evsieve --input /dev/input/by-id/keyBoard grab \
+    --map msc:scan \
+    --map key:space:1 key:leftctrl:1 key:s:1 key:s:0 key:n:1 key:n:0 key:leftctrl:0 \
+    --output  name="keyboardB4Test" create-link=/dev/input/by-id/keyBoardB4Test repeat=disable \
+    # --map key:space:1 key:leftctrl:1 key:s:1 key:s:0 key:n:1 key:n:0 key:leftctrl:0 \ not working
+    # --map key:space:1 key:leftctrl:1 key:n:1 key:n:0 key:s:1 key:s:0 key:leftctrl:0 \ working
+    # --map key:space:1 key:leftctrl:1 key:n:1 key:n:0 key:leftctrl:0 \                 working
+    # --map key:space:1 key:leftctrl:1 key:s:1 key:s:0 key:leftctrl:0 \                 working
 
 
+sudo evsieve --input /dev/input/by-id/keyBoardB4Test grab domain=regular \
+    --map key:leftshift @leftShift \
+    --hook @regular toggle=leftshiftCount:1 \
+    --hook @leftShift toggle=leftshiftCount \
+    --toggle @leftShift @leftShift0 @leftShift1 @leftShift2 @leftShift3 id=leftshiftCount \
+    --hook @leftShift3 \
+        toggle=leftshiftCount:1 send-key=key:leftshift@leftShiftUp send-key=key:a send-key=key:s send-key=key:d send-key=key:f \
+    --map @leftShiftUp key:leftshift:0 \
+    --map key @regular \
+    --output create-link=/dev/input/by-id/keyBoardTest repeat=disable \

fc8a9a36e8b3c4f368028f82df97b018bc90f5f1
yaniv <yanivprusman@gmail.com>
2025-10-29 09:42:00 +0200

    wip

--- evsieve/mappings/corsairKeyBoardLogiMouse.sh
+++ evsieve/mappings/corsairKeyBoardLogiMouse.sh
@@ -3,6 +3,7 @@ MOUSE_EVENT=$(awk '/Logitech/ && /Mouse/ {found=1} found && /Handlers/ {if (matc
 EVSIEVE_LOG_FILE=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../log/log.txt")
 SYSTEMD_LOG_FILE=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../log/error.txt")
 ECHO_LOG_FILE=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../log/echo.txt")
+SEND_KEYS=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../toggle/sendKeys")
 > "$SYSTEMD_LOG_FILE"
 for arg in "$@"; do
     if [[ "$arg" == "reset" ]]; then
@@ -34,45 +35,22 @@ systemd-run --collect --service-type=notify --unit=corsairKeyBoardLogiMouse.serv
     --toggle msc:scan:$SERVICE_INITIALIZED_CODE @serviceUnInitialized @serviceIinitialized id=serviceInitializedToggle\
     --hook "" toggle=serviceInitializedToggle:2\
     `#led numlock initialization`\
-    `#--hook @serviceUnInitialized led:numl send-key=key:numlock@send breaks-on=@serviceIinitialized`\
-    --hook @serviceUnInitialized led:numl send-key=key:a@null breaks-on=@serviceIinitialized\
-    --map key:a:1@null key:numlock:1@send key:numlock:0@send\
+    --hook @serviceUnInitialized led:numl send-key=key:a@null breaks-on=""\
+    --map key:a:1@null key:numlock:1@send key:numlock:0@send key:numlock:1@send key:numlock:0@send\
     --block key:a:0@null\
-    `#numlock toggle logic`\
-    --print key:numlock format=direct\
-    `#led numlock toggle logic`\
-    `#--toggle led:numl @ledNumUnInitialized @ledNumlInitialized id=ledNumlInitializedToggle\
-    --hook "" toggle=ledNumlInitializedToggle:2\
-    --hook @ledNumUnInitialized send-key=key:numlock@numlockInitialize\
-    --map @numlockInitialize key:numlock:1@ledNumlInitialized key:numlock:0@ledNumlInitialized`\
-    `#--copy @serviceUnInitialized key:numlock@setNumLockOnAfterInitialization %4:%4:%28`\
-    `#--map key:a@null key:a@null`\
-    `#--hook msc:scan:$SERVICE_INITIALIZED_CODE@serviceUnInitialized led:numl send-key=key:numlock@numlockInitialize sequential breaks-on=@serviceIinitialized\
-    --map @numlockInitialize key:numlock:1@numLockInitialized key:numlock:0@numLockInitialized\
-    --print led key:numlock format=direct`\
-    `#--print msc:scan:$SERVICE_INITIALIZED_CODE led format=direct`\
-    `#--map @numlockUp key:numlock:1 key:numlock:0 key:numlock:1 key:numlock:0`\
-    `#--hook`\
-    `#--print led key:numlock format=direct`\
-    `#--hook led:numl:1 key:numlock:0 send-key=key:f`\
-    `#--print @input format=direct`\
-    `#--map key:a key:numlock`\
-    `# toggle outputs`\
-    `#--hook led:numl:0 key:numlock send-key=key:numlock`\
-    `#--copy @input @unsievedOutput`\
-    `#--map @input @myOutput`\
-    `#--hook key:numlock toggle=myOutputNull toggle=unsievedOutputNull`\
-    `# --map key:numlock @dontPrint`\
-    `#--toggle @myOutput @myOutput @null id=myOutputNull`\
-    `#--toggle @unsievedOutput @null @unsievedOutput id=unsievedOutputNull`\
-    `# keyboard events`\
-    `#-print @input @null @unInitialized format=direct`\
+    `#test`\
+    --hook @serviceUnInitialized exec-shell="$SEND_KEYS 'SYN_REPORT'" breaks-on=""\
+    `#--hook @serviceUnInitialized led:numl send-key=key:a@null breaks-on=""`\
+    `#--map key:a:1@null key:numlock:1@send key:numlock:0@send\
+    --block key:a:0@null`\
+    `#set numlock on after initialization`\
+    `#test release in toggle`\
+    --print key msc:scan:~199 msc:scan:201~ format=direct\
+    `#--print @input @null @unInitialized format=direct`\
     --output @send @input @myOutput @unsievedOutput @dontPrint @setNumLockOnAfterInitialization \
     name="combined corsair keyboard and logi mouse" create-link=/dev/input/by-id/corsairKeyBoardLogiMouse repeat=disable\
     `# mouse events`\
     --map btn:forward key:enter\
-    `# toggle outputs`\
-    `# toggle outputs`\
-    --print key format=direct
+    `#--print key format=direct`
 
 
--- evsieve/toggle/sendKeys.c
+++ evsieve/toggle/sendKeys.c
@@ -5,6 +5,10 @@
 #include <string.h>
 #include <sys/time.h>
 int fd;
+const int codeForCode = 101;
+const int codeForGnomeTerminal = 102;
+const int codeForGoogleChrome = 103;
+
 void send_event(int type, int code, int value) {
     struct input_event ev;
     memset(&ev, 0, sizeof(ev));
@@ -15,26 +19,46 @@ void send_event(int type, int code, int value) {
     write(fd, &ev, sizeof(ev));
 }
 
-int getCodeForApp(const char *app) {
-    if (strcmp(app, "Code") == 0) return 102;
-    if (strcmp(app, "gnome-terminal-server") == 0) return 103;
-    if (strcmp(app, "google-chrome") == 0) return 104;
-    return 101;
+int isApp(const char *input){
+    if (strcmp(input, "Code") == 0) return codeForCode;
+    if (strcmp(input, "gnome-terminal-server") == 0) return codeForGnomeTerminal;
+    if (strcmp(input, "google-chrome") == 0) return codeForGoogleChrome;
+    return 0;
 }
+// int getCodeFromInput(const char *app) {
+//     if (strcmp(app, "Code") == 0) return 102;
+//     if (strcmp(app, "gnome-terminal-server") == 0) return 103;
+//     if (strcmp(app, "google-chrome") == 0) return 104;
+//     if (strcmp(app, "google-chrome") == 0) return 104;
+//     return 101;
+// }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         return 1;
     }
-    const char *app = argv[1];
-    const char *device = "/dev/input/by-id/corsairKeyBoardLogiMouse";
+    const char *input = argv[1];
+    // const char *device = "/dev/input/by-id/corsairKeyBoardLogiMouse";
+    const char *device = "/dev/input/by-id/usb-Corsair_CORSAIR_K100_RGB_Optical-Mechanical_Gaming_Keyboard_502A81D24AAA7CC6-event-kbd";
     fd = open(device, O_WRONLY);
     if (fd < 0) {
         return 1;
     }
-    for (int i = 0; i < 3; i++) { send_event(EV_MSC, MSC_SCAN, 100); }
-    send_event(EV_MSC, MSC_SCAN, getCodeForApp(app));
-    send_event(EV_SYN, SYN_REPORT, 0);
+    int appCode = isApp(input);
+    if (appCode){
+        for (int i = 0; i < 3; i++) { send_event(EV_MSC, MSC_SCAN, 100); }
+        send_event(EV_MSC, MSC_SCAN, appCode);
+        send_event(EV_SYN, SYN_REPORT, 0);
+    } else if (strcmp(input, "SYN_REPORT") == 0){
+        send_event(EV_SYN, SYN_REPORT, 0);
+    } else if (strcmp(input, "CodeADown") == 0){
+        send_event(EV_KEY, KEY_A, 1);
+    } else if (strcmp(input, "CodeAUP") == 0){
+        send_event(EV_KEY, KEY_A, 0);
+    } else if (strcmp(input, "CodeADownUp") == 0){
+        send_event(EV_KEY, KEY_A, 1);
+        send_event(EV_KEY, KEY_A, 0);
+    }
     close(fd);
     return 0;
 }

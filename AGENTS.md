89c04ae3b1a5f74811e3f27b1b6300a837c16355
yaniv <yanivprusman@gmail.com>
2025-10-30 16:30:45 +0200

    fixed theRealPath

--- AGENTS.md
+++ AGENTS.md
@@ -1,9 +1,1 @@
-write a program in c that i can do 
-sudo $(theCommand (name it theRealPath) /_sudoStop.sh)
-this avoids repeating code like this:
-
-SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
-
-sudo "$(realpath "${SCRIPT_DIR}/_sudoStop.sh")"
-sudo "$(realpath "${SCRIPT_DIR}/run.sh")" "$@"
-
+learn about $()
--- utilities/theRealPath.c
+++ utilities/theRealPath.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,34 +8,17 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <relative_path>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <path>\n", argv[0]);
         return 1;
     }
 
-    char resolved_path[PATH_MAX];
-    char temp_path[PATH_MAX];
-    char *dir_path;
-    
-    // Get the absolute path of the current script
-    if (realpath(argv[0], temp_path) == NULL) {
-        perror("Error resolving current script path");
+    char *absolute_path = realpath(argv[1], NULL);
+    if (!absolute_path) {
+        perror("Error resolving path");
         return 1;
     }
 
-    // Get the directory of the current script
-    dir_path = dirname(temp_path);
-
-    // Create full path by concatenating directory and provided relative path
-    snprintf(temp_path, sizeof(temp_path), "%s/%s", dir_path, argv[1]);
-
-    // Resolve the final path
-    if (realpath(temp_path, resolved_path) == NULL) {
-        perror("Error resolving final path");
-        return 1;
-    }
-
-    // Output in the format: sudo "$(realpath "path")"
-    printf("sudo \"%s\"\n", resolved_path);
-
+    printf("%s\n", absolute_path);
+    free(absolute_path);
     return 0;
 }

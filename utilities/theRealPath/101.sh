c1be676f78ae70279d6fda87290d9ed71cde208b
yaniv <yanivprusman@gmail.com>
2025-11-01 23:21:30 +0200

    wip

--- utilities/theRealPath/101.sh
+++ utilities/theRealPath/101.sh
@@ -1,2 +1,4 @@
-# theRealPath ../../ # supposed to print yaniv@ubuntu:~/coding/automateLinux/utilities/
-theRealPath ../../asdf # supposed to print yaniv@ubuntu:~/coding/automateLinux/utilities/asdf
+# current path /home/yaniv/coding/automateLinux/utilities/theRealPath/101.sh
+# theRealPath ../ # supposed to print yaniv@ubuntu:~/coding/automateLinux/utilities/
+
+theRealPath ../asdf # supposed to print yaniv@ubuntu:~/coding/automateLinux/utilities/asdf

fb47077573db9c985c8eb117bedd31cb8f432a93
yaniv <yanivprusman@gmail.com>
2025-11-06 00:33:39 +0200

    colorful ps1

--- bashrc/ps1.sh
+++ bashrc/ps1.sh
@@ -1,2 +1,31 @@
-echo "\[\e]0;\w\a\]\[\033[01;32m\]\$(if [ \"\$PWD\" = '/' ]; then echo '/'; else echo '\w/'; fi)\[\033[00m\]"
+#!/usr/bin/env bash
 
+# Start and end colors in 256-color codes
+start_color=34   # dark green
+end_color=39     # light blue
+
+# Get current working directory
+dir="${PWD/#$HOME/~}"  # optional: shorten home to ~
+
+# Length of string
+len=${#dir}
+
+# Output buffer
+prompt=""
+
+# Add terminal title
+prompt+="\[\e]0;${PWD}\a\]"
+
+# Loop through each character and assign interpolated color
+for ((i=0; i<len; i++)); do
+    # Calculate color index between start_color and end_color
+    color=$((start_color + (i * (end_color - start_color) / (len - 1))))
+    char="${dir:$i:1}"
+    prompt+="\[\033[38;5;${color}m\]$char"
+done
+
+# Reset color
+prompt+="\[\033[00m\]"
+
+# Output
+echo "$prompt/"

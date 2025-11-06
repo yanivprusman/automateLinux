b80233a46e30ed8c2f10ba43716cd187a6424748
yaniv <yanivprusman@gmail.com>
2025-11-06 09:38:25 +0200

    RGB cube 0–5

--- bashrc/aliases.sh
+++ bashrc/aliases.sh
@@ -13,3 +13,8 @@ alias cd..='cd ..'
 alias cdc='cd /home/yaniv/coding/'
 PS1_SCRIPT="$(dirname "${BASH_SOURCE[0]}")/ps1.sh"
 PROMPT_COMMAND='PS1=$(bash "'"$PS1_SCRIPT"'")'
+resetPromptColor() {
+    echo -en "\033[00m"
+}
+trap 'resetPromptColor' DEBUG
+
--- bashrc/ps1.sh
+++ bashrc/ps1.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
-
-# Start and end colors in 256-color codes
-start_color=34   # dark green
-end_color=39     # light blue
-
-# Get current working directory
-dir="${PWD/#$HOME/~}"  # optional: shorten home to ~
-
-# Length of string
+r1=5; g1=5; b1=5    # white
+r2=5; g2=5; b2=5    # white
+setStart() {
+    rgb="$1"
+    r1="${rgb:0:1}"
+    g1="${rgb:1:1}"
+    b1="${rgb:2:1}"
+}
+setEnd() {
+    rgb="$1"
+    r2="${rgb:0:1}"
+    g2="${rgb:1:1}"
+    b2="${rgb:2:1}"
+}
+setStart 555
+setEnd 030
+dir="${PWD/#$HOME/~}/"
 len=${#dir}
-
-# Output buffer
 prompt=""
-
-# Add terminal title
-prompt+="\[\e]0;${PWD}\a\]"
-
-# Loop through each character and assign interpolated color
 for ((i=0; i<len; i++)); do
-    # Calculate color index between start_color and end_color
-    color=$((start_color + (i * (end_color - start_color) / (len - 1))))
+    f=$(( i * 100 / (len - 1) ))
+    r=$(( (r1 * (100 - f) + r2 * f) / 100 ))
+    g=$(( (g1 * (100 - f) + g2 * f) / 100 ))
+    b=$(( (b1 * (100 - f) + b2 * f) / 100 ))
+    color=$((16 + 36*r + 6*g + b))
     char="${dir:$i:1}"
-    prompt+="\[\033[38;5;${color}m\]$char"
+    prompt+="\[\033[1;38;5;${color}m\]$char"
 done
-
-# Reset color
 prompt+="\[\033[00m\]"
-
-# Output
-echo "$prompt/"
+echo "$prompt"

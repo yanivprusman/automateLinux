e2142fe5443d2ab1f32e37be87222364aa655d5e
yaniv <yanivprusman@gmail.com>
2025-11-09 21:10:44 +0200

    improved err.sh error log

--- terminal/trap/err.sh
+++ terminal/trap/err.sh
@@ -2,13 +2,17 @@ exit_code=$?
 cmd="${BASH_COMMAND}"
 line_no=${BASH_LINENO[0]}
 func="${FUNCNAME[1]}"
-# date_time
 date_time=$(date '+%Y-%m-%d %H:%M:%S')
+error_msg=$(2>&1 eval "$cmd" >/dev/null || true)
 {
     echo "$date_time Exit code: $exit_code Command: $cmd Line: $line_no Function: ${func:-main} PWD: $PWD"
-    echo "Script:"
+    if [ -n "$error_msg" ]; then
+        echo "Error message: $error_msg"
+    fi
+    echo "Stack:"
     for f in "${BASH_SOURCE[@]}"; do
         echo "  $f"
     done
     echo "------------------------------------------"
 } >> "$AUTOMATE_LINUX_TRAP_ERR_LOG_FILE"
+

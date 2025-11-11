d3e8432625b90b999e7b824e6a2844a626c77bcd
yaniv <yanivprusman@gmail.com>
2025-11-11 18:51:07 +0200

    symlinked desktop

--- terminal/tests/testDirHistory.sh
+++ terminal/tests/testDirHistory.sh
@@ -1,10 +1,12 @@
-exec > "${AUTOMATE_LINUX_DIR_HISTORY_TESTS_DIR}terminaloutput.txt" 2>&1
-gnome-terminal --tab  
-rm "${AUTOMATE_LINUX_DIR_HISTORY_DIR}"* #2>/dev/null
-cd ~/
-cd coding/automateLinux/terminal/tests
-cd ..
-cd ..
-${AUTOMATE_LINUX_DIR_HISTORY_TESTS_DIR}printDirHistory.sh > "${AUTOMATE_LINUX_DIR_HISTORY_TESTS_DIR}output.txt"
+#!/bin/bash
+ls
+# exec > "${AUTOMATE_LINUX_DIR_HISTORY_TESTS_DIR}terminaloutput.txt" 2>&1
+# gnome-terminal --tab  
+# rm "${AUTOMATE_LINUX_DIR_HISTORY_DIR}"* #2>/dev/null
+# cd ~/
+# cd coding/automateLinux/terminal/tests
+# cd ..
+# cd ..
+# ${AUTOMATE_LINUX_DIR_HISTORY_TESTS_DIR}printDirHistory.sh > "${AUTOMATE_LINUX_DIR_HISTORY_TESTS_DIR}output.txt"
 
 

b859beb60e466477d1ba2ea9fa8e92515e1b3422
yaniv <yanivprusman@gmail.com>
2025-11-12 18:51:04 +0200

    wip testDirHistory

--- terminal/tests/101.sh
+++ terminal/tests/101.sh
@@ -1,3 +1,9 @@
+green='\e[32m'
+yellow='\e[33m'
+NC='\e[0m' # No Color
+echo -e "${green}testDirHistory.sh:${NC}"
+cat testDirHistory.sh | awk 'NR >= 3'
 heredoc testDirHistory.sh
-./testDirHistory.HereDoc 
+./testDirHistory.hereDoc 
+echo -e "${yellow}$AUTOMATE_LINUX_PRINT_BLOCK_SEPARATOR${NC}"
 cat "${AUTOMATE_LINUX_DATA_DIR}terminaloutput1.txt"
--- terminal/tests/testDirHistory.sh
+++ terminal/tests/testDirHistory.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-# ls > "${AUTOMATE_LINUX_DIR_HISTORY_TESTS_DIR}output.txt"
 exec > "${AUTOMATE_LINUX_DATA_DIR}terminaloutput1.txt" 2>&1
 set -x
 rm "${AUTOMATE_LINUX_DIR_HISTORY_DIR}"* #2>/dev/null

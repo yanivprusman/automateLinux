8eaf3df386f64d6325665bb9660f4f6c26abdc46
yaniv <yanivprusman@gmail.com>
2025-11-12 09:53:52 +0200

    wip

--- terminal/tests/testDirHistory.sh
+++ terminal/tests/testDirHistory.sh
@@ -1,12 +1,25 @@
+
 #!/bin/bash
-ls > "${AUTOMATE_LINUX_DIR_HISTORY_TESTS_DIR}output.txt"
-# exec > "${AUTOMATE_LINUX_DIR_HISTORY_TESTS_DIR}terminaloutput.txt" 2>&1
+# ls > "${AUTOMATE_LINUX_DIR_HISTORY_TESTS_DIR}output.txt"
+exec > "${AUTOMATE_LINUX_DIR_HISTORY_TESTS_DIR}terminaloutput1.txt" 2>&1
+set -x
+rm "${AUTOMATE_LINUX_DIR_HISTORY_DIR}"* #2>/dev/null
+cd ~/
+cd coding/automateLinux/terminal/tests
+cd ..
+cd ..
+${AUTOMATE_LINUX_DIR_HISTORY_TESTS_DIR}printDirHistory.sh 
+
+
+bash <<'EOF'
+exec > "${AUTOMATE_LINUX_DIR_HISTORY_TESTS_DIR}terminaloutput2.txt" 2>&1
+set -x
+rm "${AUTOMATE_LINUX_DIR_HISTORY_DIR}"* #2>/dev/null
+cd ~/
+cd coding/automateLinux/terminal/tests
+cd ..
+cd ..
+${AUTOMATE_LINUX_DIR_HISTORY_TESTS_DIR}printDirHistory.sh 
+EOF
+
 # gnome-terminal --tab  
-# rm "${AUTOMATE_LINUX_DIR_HISTORY_DIR}"* #2>/dev/null
-# cd ~/
-# cd coding/automateLinux/terminal/tests
-# cd ..
-# cd ..
-# ${AUTOMATE_LINUX_DIR_HISTORY_TESTS_DIR}printDirHistory.sh > "${AUTOMATE_LINUX_DIR_HISTORY_TESTS_DIR}output.txt"
-
-

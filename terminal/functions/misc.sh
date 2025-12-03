daed2e07d8b8675f729d57abf95fbb10101a0f6a
yaniv <yanivprusman@gmail.com>
2025-12-03 23:53:01 +0200

    wip added setCdc

--- terminal/functions/misc.sh
+++ terminal/functions/misc.sh
@@ -17,3 +17,18 @@ lstr(){
     ls --color=always "$@" | tr " " "\n"
 }
 export -f lstr
+
+copyToClipboard(){
+    (xclip -selection clipboard)
+}
+export -f copyToClipboard
+
+setCdc() {
+    local newDir="$1"
+    aliasFile="${AUTOMATE_LINUX_TERMINAL_DIR}aliases.sh"
+    if grep -q '^alias cdc=' "$aliasFile"; then
+        sed -i "s|^alias cdc=.*|alias cdc='cd \"$newDir\"'|" "$aliasFile"
+    fi
+    alias cdc="cd \"$newDir\""
+}
+export -f setCdc

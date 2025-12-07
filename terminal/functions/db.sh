b8a6ed0deb9095fff969e20c0ff82128e655c3c2
yaniv <yanivprusman@gmail.com>
2025-12-07 20:10:40 +0200

    cleanup

--- terminal/functions/db.sh
+++ terminal/functions/db.sh
@@ -1,20 +1,7 @@
-# watchDb() {
-#     watch -d -n 1 "sqlite3 \"$AUTOMATE_LINUX_DIR/data/daemon.db\" 'SELECT * FROM kv;'"
-# }
-
-# watchDb() {
-#     inotifywait -m -e modify "$AUTOMATE_LINUX_DIR/data/daemon.db" |
-#     while read -r _; do
-#         . "${AUTOMATE_LINUX_DAEMON_DIR}dbChanged.sh"
-#     done
-#     #     d printDirHistory > "${AUTOMATE_LINUX_DIR}/data/watchDB.txt"
-#     #     # sqlite3 "$AUTOMATE_LINUX_DIR/data/daemon.db" "SELECT * FROM kv;" > "${AUTOMATE_LINUX_DIR}/data/watchDB.txt"
-
-# }
-# export -f watchDb
 
 watchDb() {
-    while inotifywait -e modify "${AUTOMATE_LINUX_DIR}data/daemon.db"; do
+    while inotifywait -e modify "${AUTOMATE_LINUX_DIR}data/daemon.db" > /dev/null 2>&1; do
         d printDirHistory > "${AUTOMATE_LINUX_DIR}data/watchDB.txt"
+        # d printDirHistory
     done
 }

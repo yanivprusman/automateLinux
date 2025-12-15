8490a2117280c4bd417169172549d1d31875f69e
yaniv <yanivprusman@gmail.com>
2025-12-15 14:26:35 +0200

    working sudoCommands.sh, tested with print

--- terminal/sudoCommands/sudoCommands.sh
+++ terminal/sudoCommands/sudoCommands.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-echo $SUDO_COMMAND
-$(SUDO_COMMAND) $@
-
+localFunction(){
+    local script_name=$(basename ${SUDO_COMMAND%% *} )
+    if [[ "$script_name" == "print" ]]; then
+        # source ../functions/printDir.sh
+        . $(theRealPath ../functions/printDir.sh )
+        print $@
+    else
+        :
+        # source ../functions/"$script_name".sh
+    fi
+}
+localFunction "$@"
+unset localFunction
 # . $(theRealPath ../functions/printDir.sh -debug)
 # print $@
 

088e15d6feea873f82301f5e66f90837e9efa8ce
yaniv <yanivprusman@gmail.com>
2025-12-25 01:04:10 +0200

    detecting G keys working

--- evsieve/mappings/corsairKeyBoardLogiMouseAll.sh
+++ evsieve/mappings/corsairKeyBoardLogiMouseAll.sh
@@ -1,8 +1,9 @@
 evsieve --input $keyboardPath $mousePath grab domain=input \
 --map key @keyboard                                              `#part of detecting window`\
 --map btn:forward key:enter `#mouse to enter`\
---hook key:leftctrl key:1 exec-shell='sudo -u yaniv DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus DISPLAY=:0 notify-send "hi" "5"'`#sanity check` \
+--hook key:leftctrl key:1 exec-shell='sudo -u yaniv DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus DISPLAY=:0 notify-send "hi" "6"'`#sanity check` \
 --copy key @gprogress \
+--toggle @gprogress @gprogress1 @gprogress2 @gprogress3 @gprogress4 @gprogress5 id=GToggle \
 --hook msc:scan:$codeForAppCodes toggle=appCodesToggle         `#detecting window continued`\
 --map msc:scan:$codeForAppCodes @codeForAppCodesCount                                       \
 --toggle @codeForAppCodesCount @pressed1 @pressed2 @pressed3 id=appCodesToggle              \
@@ -20,10 +21,16 @@ evsieve --input $keyboardPath $mousePath grab domain=input \
 --hook key:leftctrl@gprogress3 toggle=GToggle:4 \
 --hook @gprogress4 toggle=GToggle:1 \
 --hook key:leftshift@gprogress4 toggle=GToggle:5 \
-`#--hook key`\
---toggle @gprogress @gprogress1 @gprogress2 @gprogress3 @gprogress4 @gprogress5 @gprogress6 id=GToggle \
---toggle @gKey @gKey1 @gKey2 @gKey3 @gKey4 @gKey5 @gKey6 \
---print format=direct \
+--hook @gprogress5 toggle=GToggle:1 \
+--print @nothing \
+--hook key:1@gprogress5 exec-shell='sudo -u yaniv DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus DISPLAY=:0 notify-send "hi" "1"' \
+--withhold \
+--hook key:2@gprogress5 exec-shell='sudo -u yaniv DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus DISPLAY=:0 notify-send "hi" "2"' \
+--hook key:3@gprogress5 exec-shell='sudo -u yaniv DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus DISPLAY=:0 notify-send "hi" "3"' \
+--hook key:4@gprogress5 exec-shell='sudo -u yaniv DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus DISPLAY=:0 notify-send "hi" "4"' \
+--hook key:5@gprogress5 exec-shell='sudo -u yaniv DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus DISPLAY=:0 notify-send "hi" "5"' \
+--hook key:6@gprogress5 exec-shell='sudo -u yaniv DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus DISPLAY=:0 notify-send "hi" "6"' \
+--print  format=direct \
 --output create-link=/dev/input/by-id/corsairKeyBoardLogiMouse 2>&1
 
 

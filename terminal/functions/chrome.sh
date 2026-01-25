57fc061d4d8f7effe8316f5ef2d40d19ed0a696e
yaniv <yanivprusman@gmail.com>
2026-01-25 18:21:25 +0200

    wip

--- terminal/functions/chrome.sh
+++ terminal/functions/chrome.sh
@@ -1,6 +1,6 @@
-enableChromeAsRoot(){
-    useradd chrome -s /bin/bash -d /home/chrome -m
-}
+# enableChromeAsRoot(){
+#     useradd chrome -s /bin/bash -d /home/chrome -m
+# }
 chromeAsRoot(){
     google-chrome --no-sandbox --user-data-dir=/root/chrome
 }
